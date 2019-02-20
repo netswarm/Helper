f7a1b7a5d679f0d9fb74f1b4e2b4043e6d5658ba
khalil <kingjamezsavior@gmail.com>
2019-02-20 14:39:42 -0500

    Added setter/getter methods

--- Goal.java
+++ Goal.java
@@ -1,12 +1,33 @@
+/**
+* Class file for goal that sets the length
+* for the goal as well as a description and
+* contains a list of milestones in order to
+* achieve said goal.
+*/
 import java.util.*;
 
 public class Goal
 {
-  String s-date;
-  String e-date;
-  String name;
-  String desxription;
+  int length;//length of the goal set in months
+  String name;//name of the Goal
+  String description;//description of the goal
 
-  public
+  public void setLength(int months)//sets length of goal
+  { months = length; }
+
+  public int getLength()//returns length of goal to main
+  { return length; }
+
+  public void setName(String title)
+  { name = title; }
+
+  public String getName()
+  { return name; }
+
+  public void setDescription(String desc)
+  { desc = description }
+
+  public String getDescription()
+  { return description; }
 
 }
