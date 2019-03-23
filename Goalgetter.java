49c73d4aa764059efc153a9ad30e39cc94efba55
khalil <kingjamezsavior@gmail.com>
2019-03-22 23:56:29 -0400

    Added individual menu and goal methodsand fixed syntax errors

--- Goalgetter.java
+++ Goalgetter.java
@@ -3,13 +3,37 @@ import java.util.*;
 public class Goalgetter{
   public static void main(String[] args)
   {
+    do{
+        int ans = menu();
+        if (ans = 1)
+        { addGoals(); }
+
+        if (ans = 2)
+        { printGoals(); }
+      } 
+  }
+
+  public static int menu()//menu class to help with navigation of program in cli
+  {
+    int ans = 0;
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("\nWhat would you like to do today?\n" + " 1. Create Goal\n"
+    + " 2. review goals\n" + " 3. review tasks\n");
+
+    ans = in.nextInt();
+    return ans;
+  }
+
+  public static void addGoals()// adds goals to ArrayList for review and modification
+  {
     String title;
     String timeframe;
     String description;
     char ans;
 
-    Scanner k = new Scanner(System.in);
     ArrayList<Goal> better = new ArrayList<Goal>();
+    Scanner k = new Scanner(System.in);
 
     System.out.println("Let's add up to 3 goals to work on!");
     do{
@@ -29,10 +53,17 @@ public class Goalgetter{
         ans = k.nextLine().charAt(0);
       }
       while (ans == 'y');
-    if (ans == 'n')
-    {
-      better.get(0).displayGoal();
-    }
   }
 
+  public static void printGoals(ArrayList copy)//prints out lists of goals for user
+  {
+    int count = 0
+    int length = copy.size();
+
+    while (count < length)
+    {
+      copy[int].displayGoal();
+      int++;
+    }
+  }
 }
